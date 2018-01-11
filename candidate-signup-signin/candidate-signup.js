d0913281e7ec893614f4d1b6959e1073e2e9c38b
Sushil <sushil@goodservice.in>
2018-01-11 13:14:21 +0530

    add signup and login testcases

--- candidate-signup-signin/candidate-signup.js
+++ candidate-signup-signin/candidate-signup.js
@@ -7,7 +7,8 @@ var should = chai.should();
 // This test set is for not login condition
 module.exports = {
   // Testing the page element like body, search box, logo and link, text, heading, footer and footer links
-  'Home': function (browser) {
+  'Signup': function (browser) {
+    var menu = '.links .links-item';
     browser.url(browser.launch_url);
     browser.waitForElementPresent('body', 5000);
     browser.assert.title('Landing - the best tech jobs in 1 place');
@@ -36,24 +37,52 @@ module.exports = {
     browser.expect.element('.main-footer .container a:nth-of-type(2)').text.to.contain('LANDING .CO');
     browser.expect.element('.main-footer .container a:nth-of-type(2)').to.have.attribute('href').which.contains('/');
     browser.saveScreenshot('./screenshots/expect-home.png');
-    browser.end();
-  },
-  // Testing the navigation menu elements and corresponding links
-  'Navigation': function (browser) {
-    var menu = '.links .links-item';
-    function testSidebar(items) {
+    function navigation(items) {
       expect(items.value.length).to.equal(2); // Chai module
       browser.expect.element(menu + ':nth-of-type(1) a span').text.to.contain('FOR CANDIDATES');
       browser.expect.element(menu + ':nth-of-type(1) a').to.have.attribute('href').which.contains('/user/sign_up');
       browser.expect.element(menu + ':nth-of-type(2) a span').text.to.contain('FOR RECRUITERS');
       browser.expect.element(menu + ':nth-of-type(2) a').to.have.attribute('href').which.contains('/user/sign_up?is_recruiter=true');
     }
-    browser.url(browser.launch_url);
-    browser.waitForElementPresent('body', 1000);
     browser.expect.element(menu).to.be.present;
     browser.expect.element(menu).to.be.visible;
-    browser.elements('css selector', menu, testSidebar);
-    browser.saveScreenshot('./screenshots/expect-home-nav.png');
+    browser.elements('css selector', menu, navigation);
+    function navigationAfterLogin(items) {
+      browser.expect.element(menu + ' .profile-link a.dropdown-toggle  > span.d-block').text.to.contain('MY PROFILE');
+       browser.click(menu + ' .profile-link a.dropdown-toggle  > span.d-block');
+      browser.expect.element(menu + ':nth-of-type(1) .profile-link ul.dropdown-menu').to.be.present;
+      browser.expect.element(menu + ':nth-of-type(1) .profile-link ul.dropdown-menu li:nth-of-type(1) a').to.present;
+      browser.expect.element(menu + ':nth-of-type(1) .profile-link ul.dropdown-menu li:nth-of-type(1) a').to.have.attribute('href').which.contains('/profile/edit');
+      browser.expect.element(menu + ':nth-of-type(1) .profile-link ul.dropdown-menu li:nth-of-type(1) a span').to.present;
+      browser.expect.element(menu + ':nth-of-type(1) .profile-link ul.dropdown-menu li:nth-of-type(1) a span').to.be.visible;
+      browser.expect.element(menu + ':nth-of-type(1) .profile-link ul.dropdown-menu li:nth-of-type(1) a span').text.to.equal('My Profile');
+      browser.expect.element(menu + ':nth-of-type(1) .profile-link ul.dropdown-menu li:nth-of-type(2) a').to.present;
+      browser.expect.element(menu + ':nth-of-type(1) .profile-link ul.dropdown-menu li:nth-of-type(2) a').to.have.attribute('href').which.contains('/user/sign_out');
+      browser.expect.element(menu + ':nth-of-type(1) .profile-link ul.dropdown-menu li:nth-of-type(2) a span').to.present;
+      browser.expect.element(menu + ':nth-of-type(1) .profile-link ul.dropdown-menu li:nth-of-type(2) a > span').to.be.visible;
+      browser.expect.element(menu + ':nth-of-type(1) .profile-link ul.dropdown-menu li:nth-of-type(2) a span').text.to.equal('Logout');
+      browser.expect.element(menu + ':nth-of-type(2) a span').text.to.contain('FOR RECRUITERS');
+      browser.expect.element(menu + ':nth-of-type(2) a').to.have.attribute('href').which.contains('/user/sign_up?is_recruiter=true');
+    }
+    browser.click('a.ga-trackable');
+    browser.expect.element('.page-popup-like .card-box h2.mb-15').text.to.equal('Candidate Signup');
+    browser.waitForElementPresent('body', 2000);
+    browser.setValue('input[type=email]', 'sushilkundu143+24@gmail.com');
+    browser.setValue('input[type=password]', 'xxx');
+    browser.click('input[type=submit]');
+    browser.pause(5000);
+    browser.url('https://www.gmail.com');
+    browser.setValue('input[type=email]', 'xxx@gmail.com');
+    browser.click('.CwaK9');
+    browser.pause(2000);
+    browser.setValue('input[type=password]', 'xxx');
+    browser.click('.CwaK9');
+    browser.pause(25000);
+    browser.click('.CwaK9');
+    browser.waitForElementPresent('body', 5000);
+    browser.click('span.yP');
+    browser.elements('css selector', menu, navigationAfterLogin);
+    browser.assert.urlEquals(browser.launch_url + '/j', 'Candidate signup Sucessful.');
     browser.end();
   }
 };

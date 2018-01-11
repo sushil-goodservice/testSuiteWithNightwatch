df11903b1dd171127e7b288322e50f72c9de7a77
Sushil <sushil@goodservice.in>
2018-01-11 18:22:20 +0530

    add candidate-login-signup cases

--- candidate-signup-signin/candidate-signup-with-google-plus.js
+++ candidate-signup-signin/candidate-signup-with-google-plus.js
@@ -7,7 +7,8 @@ var should = chai.should();
 // This test set is for not login condition
 module.exports = {
   // Testing the page element like body, search box, logo and link, text, heading, footer and footer links
-  'Home': function (browser) {
+  'Signup': function (browser) {
+    var menu = '.links .links-item';
     browser.url(browser.launch_url);
     browser.waitForElementPresent('body', 5000);
     browser.assert.title('Landing - the best tech jobs in 1 place');
@@ -36,24 +37,114 @@ module.exports = {
     browser.expect.element('.main-footer .container a:nth-of-type(2)').text.to.contain('LANDING .CO');
     browser.expect.element('.main-footer .container a:nth-of-type(2)').to.have.attribute('href').which.contains('/');
     browser.saveScreenshot('./screenshots/expect-home.png');
-    browser.end();
-  },
-  // Testing the navigation menu elements and corresponding links
-  'Navigation': function (browser) {
-    var menu = '.links .links-item';
-    function testSidebar(items) {
+    function navigation(items) {
       expect(items.value.length).to.equal(2); // Chai module
       browser.expect.element(menu + ':nth-of-type(1) a span').text.to.contain('FOR CANDIDATES');
       browser.expect.element(menu + ':nth-of-type(1) a').to.have.attribute('href').which.contains('/user/sign_up');
       browser.expect.element(menu + ':nth-of-type(2) a span').text.to.contain('FOR RECRUITERS');
       browser.expect.element(menu + ':nth-of-type(2) a').to.have.attribute('href').which.contains('/user/sign_up?is_recruiter=true');
     }
-    browser.url(browser.launch_url);
-    browser.waitForElementPresent('body', 1000);
     browser.expect.element(menu).to.be.present;
     browser.expect.element(menu).to.be.visible;
-    browser.elements('css selector', menu, testSidebar);
-    browser.saveScreenshot('./screenshots/expect-home-nav.png');
+    browser.elements('css selector', menu, navigation);
+    function navigationAfterLogin(items) {
+      browser.expect.element(menu + ' .profile-link a.dropdown-toggle  > span.d-block').text.to.contain('MY PROFILE');
+       browser.click(menu + ' .profile-link a.dropdown-toggle  > span.d-block');
+      browser.expect.element(menu + ':nth-of-type(1) .profile-link ul.dropdown-menu').to.be.present;
+      browser.expect.element(menu + ':nth-of-type(1) .profile-link ul.dropdown-menu li:nth-of-type(1) a').to.present;
+      browser.expect.element(menu + ':nth-of-type(1) .profile-link ul.dropdown-menu li:nth-of-type(1) a').to.have.attribute('href').which.contains('/profile/edit');
+      browser.expect.element(menu + ':nth-of-type(1) .profile-link ul.dropdown-menu li:nth-of-type(1) a span').to.present;
+      browser.expect.element(menu + ':nth-of-type(1) .profile-link ul.dropdown-menu li:nth-of-type(1) a span').to.be.visible;
+      browser.expect.element(menu + ':nth-of-type(1) .profile-link ul.dropdown-menu li:nth-of-type(1) a span').text.to.equal('My Profile');
+      browser.expect.element(menu + ':nth-of-type(1) .profile-link ul.dropdown-menu li:nth-of-type(2) a').to.present;
+      browser.expect.element(menu + ':nth-of-type(1) .profile-link ul.dropdown-menu li:nth-of-type(2) a').to.have.attribute('href').which.contains('/user/sign_out');
+      browser.expect.element(menu + ':nth-of-type(1) .profile-link ul.dropdown-menu li:nth-of-type(2) a span').to.present;
+      browser.expect.element(menu + ':nth-of-type(1) .profile-link ul.dropdown-menu li:nth-of-type(2) a > span').to.be.visible;
+      browser.expect.element(menu + ':nth-of-type(1) .profile-link ul.dropdown-menu li:nth-of-type(2) a span').text.to.equal('Logout');
+      browser.expect.element(menu + ':nth-of-type(2) a span').text.to.contain('FOR RECRUITERS');
+      browser.expect.element(menu + ':nth-of-type(2) a').to.have.attribute('href').which.contains('/user/sign_up?is_recruiter=true');
+    }
+    browser.click('a.ga-trackable');
+    browser.waitForElementPresent('body', 2000);
+    browser.expect.element('.page-popup-like').to.be.present;
+    browser.expect.element('.page-popup-like').to.be.visible;
+    browser.expect.element('.page-popup-like .card-box h2.mb-15').text.to.equal('Candidate Signup');
+    browser.expect.element('label[for=user_email]').to.be.present;
+    browser.expect.element('label[for=user_email]').to.be.visible;
+    browser.expect.element('label[for=user_email]').text.to.contain('Email');
+    browser.expect.element('input[id=user_email]').to.be.present;
+    browser.expect.element('input[id=user_email]').to.be.visible;
+    browser.expect.element('label[for=user_password]').to.be.present;
+    browser.expect.element('label[for=user_password]').to.be.visible;
+    browser.expect.element('label[for=user_password]').text.to.contain('Set your password');
+    browser.expect.element('input[id=user_password]').to.be.present;
+    browser.expect.element('input[id=user_password]').to.be.visible;
+    browser.expect.element('input[name=commit]').to.be.present;
+    browser.expect.element('input[name=commit]').to.be.visible;
+    browser.expect.element('input[name=commit]').to.have.value.that.equals('Submit');
+    browser.expect.element('.pt-2 a:nth-of-type(1).btn-googleplus').to.be.present;
+    browser.expect.element('.pt-2 a:nth-of-type(1).btn-googleplus').to.be.visible;
+    browser.expect.element('.pt-2 a:nth-of-type(1).btn-googleplus').text.to.contain('Login with Google+');
+    browser.assert.attributeContains('.pt-2 a.btn-googleplus', 'href', '/user/auth/google_oauth2');
+    browser.expect.element('.pt-2 a:nth-of-type(2)').to.be.present;
+    browser.expect.element('.pt-2 a:nth-of-type(2)').to.be.visible;
+    browser.expect.element('.pt-2 a:nth-of-type(2)').text.to.contain('Already registered? Click here to Login');
+    browser.assert.attributeContains('.pt-2 a:nth-of-type(2)', 'href', '/user/sign_in');
+    browser.expect.element('.pt-2 a:nth-of-type(3)').to.be.present;
+    browser.expect.element('.pt-2 a:nth-of-type(3)').to.be.visible;
+    browser.expect.element('.pt-2 a:nth-of-type(3)').text.to.contain('Resend Email Confirmation');
+    browser.assert.attributeContains('.pt-2 a:nth-of-type(3)', 'href', '/user/confirmation/new');
+    browser.expect.element('.pt-2 a:nth-of-type(4)').to.be.present;
+    browser.expect.element('.pt-2 a:nth-of-type(4)').to.be.visible;
+    browser.expect.element('.pt-2 a:nth-of-type(4)').text.to.contain('Recruiter Signup');
+    browser.assert.attributeContains('.pt-2 a:nth-of-type(4)', 'href', '/user/sign_up?is_recruiter=true');
+    browser.click('a.block.text-blue');
+    browser.waitForElementPresent('body', 2000);
+    browser.expect.element('.page-popup-like .card-box h2.mb-15').text.to.equal('Candidate Login');
+    browser.expect.element('label[for=user_email]').to.be.present;
+    browser.expect.element('label[for=user_email]').to.be.visible;
+    browser.expect.element('label[for=user_email]').text.to.contain('Email');
+    browser.expect.element('input[id=user_email]').to.be.present;
+    browser.expect.element('input[id=user_email]').to.be.visible;
+    browser.expect.element('label[for=user_password]').to.be.present;
+    browser.expect.element('label[for=user_password]').to.be.visible;
+    browser.expect.element('label[for=user_password]').text.to.contain('Password');
+    browser.expect.element('input[id=user_password]').to.be.present;
+    browser.expect.element('input[id=user_password]').to.be.visible;
+    browser.expect.element('input[name=commit]').to.be.present;
+    browser.expect.element('input[name=commit]').to.be.visible;
+    browser.expect.element('input[name=commit]').to.have.value.that.equals('Login');
+    browser.expect.element('.f-pwd').to.be.present;
+    browser.expect.element('.f-pwd').to.be.visible;
+    browser.expect.element('.f-pwd').text.to.contain('(Forgot your password?)');
+    browser.assert.attributeContains('.f-pwd', 'href', '/user/password/new');
+    browser.expect.element('.pt-2 a:nth-of-type(1).btn-googleplus').to.be.present;
+    browser.expect.element('.pt-2 a:nth-of-type(1).btn-googleplus').to.be.visible;
+    browser.expect.element('.pt-2 a:nth-of-type(1).btn-googleplus').text.to.contain('Login with Google+');
+    browser.assert.attributeContains('.pt-2 a.btn-googleplus', 'href', '/user/auth/google_oauth2');
+    browser.expect.element('.pt-2 a:nth-of-type(2)').to.be.present;
+    browser.expect.element('.pt-2 a:nth-of-type(2)').to.be.visible;
+    browser.expect.element('.pt-2 a:nth-of-type(2)').text.to.contain('New User? Click here to Signup');
+    browser.assert.attributeContains('.pt-2 a:nth-of-type(2)', 'href', '/user/sign_up');
+    browser.expect.element('.pt-2 a:nth-of-type(3)').to.be.present;
+    browser.expect.element('.pt-2 a:nth-of-type(3)').to.be.visible;
+    browser.expect.element('.pt-2 a:nth-of-type(3)').text.to.contain('Resend Email Confirmation');
+    browser.assert.attributeContains('.pt-2 a:nth-of-type(3)', 'href', '/user/confirmation/new');
+    browser.expect.element('.pt-2 a:nth-of-type(4)').to.be.present;
+    browser.expect.element('.pt-2 a:nth-of-type(4)').to.be.visible;
+    browser.expect.element('.pt-2 a:nth-of-type(4)').text.to.contain('Recruiter Login');
+    browser.assert.attributeContains('.pt-2 a:nth-of-type(4)', 'href', '/user/sign_in?is_recruiter=true');
+    browser.click('.pt-2 a:nth-of-type(1).btn-googleplus');
+    browser.waitForElementPresent('body', 5000);
+    //browser.url('https://www.gmail.com');
+    browser.setValue('input[type=email]', 'kapur.r1985@gmail.com');
+    browser.click('.CwaK9');
+    browser.pause(2000);
+    browser.setValue('input[type=password]', 'Kapur.rahul');
+    browser.click('.CwaK9');
+    browser.waitForElementVisible('body', 6000);
+    browser.elements('css selector', menu, navigationAfterLogin);
+    browser.assert.urlEquals(browser.launch_url + '/j#', 'Candidate login Sucessful.');
     browser.end();
   }
 };

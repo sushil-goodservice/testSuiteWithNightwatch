df63767b80986e5ac9628c23d19b5790cee1c8e8
Sushil Kundu <sushil@goodservice.in>
2018-02-13 13:51:22 +0530

    add mobile test cases for candidate header

--- candidate-home-page-header/candidate-home-page-header-link-not-login.js
+++ candidate-home-page-header/candidate-home-page-header-link-not-login.js
@@ -9,42 +9,41 @@ module.exports = {
   // Testing the page element like body, search box, logo and link, text, heading, footer and footer links
   'Home Page Navigation Not Login': function (browser) {
     browser.url(browser.launch_url); // redirect to home page url
-    browser.waitForElementPresent('body', 5000); // wait for page body should be loaded.
+    browser.waitForElementPresent('body.mobile-visible', 5000); // wait for page body should be loaded.
     browser.assert.title('Landing - the best tech jobs in 1 place'); // match the home page title
+    browser.expect.element('header.mobile-visible').to.be.present; // check mobile header should be present
+    browser.expect.element('.header.mobile-visible').to.be.visible; // check mobile header should be visible
+    browser.expect.element('#user-mobile-header .header-wrapper').to.be.present; // check mobile header inner div should be present
+    browser.expect.element('#user-mobile-header .header-wrapper').to.be.visible; // check mobile header inner div should be visible
     // checkHomePageElementsTextLink: define function to test all the elements present in home page. 
     var homePageElements = [{
+        element: '.mob-dropdown',
+        text: '',
+        link: ''
+      },
+      {
         element: '.header-logo',
         text: '',
         link: ''
       },
       {
-        element: '#user-header-search',
+        element: '.mob-search-link',
         text: '',
         link: ''
       },
       {
-        element: '.page-heading',
+        element: 'a.border-top.new-rec-btn',
+        text: 'Recruiters',
+        link: '/user/sign_up?is_recruiter=true'
+      },
+      {
+        element: 'form.candidate-search-wrap',
         text: '',
         link: ''
       },
       {
-        element: '.header-logo a',
-        text: 'LANDING .CO',
-        link: '/'
-      },
-      {
-        element: '.page-heading h2',
-        text: 'Browse Jobs',
-        link: ''
-      },
-      {
-        element: '.popular-tags',
+        element: '.content-box',
         text: '',
-        link: ''
-      },
-      {
-        element: '.popular-tags .popular-tags-title',
-        text: 'Popular Search Tags:',
         link: ''
       },
       {
@@ -69,9 +68,9 @@ module.exports = {
       }
     ];
     // check the page elements are present, visible, having the following text with the following  as mention the array.
-    // Home Page element check function start here 
+    // Home (Candidate Home) Page element check function start here 
     function checkHomePageElementsTextLink(homePageElements) {
-      for (x in homePageElements) {
+      for (var x in homePageElements) {
         var currentHomePageElements = homePageElements[x];
         if (currentHomePageElements.text != '' && currentHomePageElements.link != '') {
           browser.expect.element(currentHomePageElements.element).to.be.present;
@@ -89,31 +88,52 @@ module.exports = {
       }
     }
     checkHomePageElementsTextLink(homePageElements);
-    // Home Page element check function end here 
+    // Home (Candidate Home) Page element check function end here 
     // check the candidate header navigation link when not login start here
-    var menu = '.links .links-item'; // menu element
+    var menu = '#user-mobile-header .mob-dropdown .options-wrap .options-container'; // mobile menu element
     var menuNotLoginElements = [{
-        element: menu + ':nth-of-type(1) a',
-        text: 'FOR CANDIDATES',
+        element: menu + ' .auxiliary-item h6.options-title',
+        text: 'Menu'
+      },
+      {
+        element: menu + ' .select-item a',
+        text: 'Sign Up',
         link: '/user/sign_up'
       },
       {
-        element: menu + ':nth-of-type(2) a',
-        text: 'FOR RECRUITERS',
+        element: menu + ' .select-item a',
+        text: 'Login',
+        link: '/user/sign_in'
+      },
+      {
+        element: menu + ' .select-item a',
+        text: 'Recruiters',
         link: '/user/sign_up?is_recruiter=true'
       }
     ];
-
+    browser.click('#mobMenuToggler', function (response) {
+      this.assert.ok(browser === this, 'Menu button clicked.');
+      browser.waitForElementPresent(menu, 2000);
+      browser.waitForElementVisible(menu, 2000);
+    });
+    browser.pause(1000);
     function navigation(menu, menuNotLoginElements) {
       browser.expect.element(menu).to.be.present;
       browser.expect.element(menu).to.be.visible;
-      for (i in menuNotLoginElements) {
+      for (var i in menuNotLoginElements) {
         var currentMenuNotLoginElements = menuNotLoginElements[i];
-        browser.expect.element(currentMenuNotLoginElements.element).text.to.contain(currentMenuNotLoginElements.text);
-        browser.expect.element(currentMenuNotLoginElements.element).to.have.attribute('href').which.contains(currentMenuNotLoginElements.href);
+        browser.expect.element(currentMenuNotLoginElements.element).to.be.present;
+        browser.expect.element(currentMenuNotLoginElements.element).to.be.visible;
+        if (currentMenuNotLoginElements.hasOwnProperty('link')) {
+          browser.expect.element(currentMenuNotLoginElements.element).text.to.contain(currentMenuNotLoginElements.text);
+          browser.expect.element(currentMenuNotLoginElements.element).to.have.attribute('href').which.contains(currentMenuNotLoginElements.link);
+        } else {
+          browser.expect.element(currentMenuNotLoginElements.element).text.to.contain(currentMenuNotLoginElements.text);
+        }
       }
     }
     navigation(menu, menuNotLoginElements);
+    browser.pause(3000);
     // candidate header navigation link check end here when not login 
     browser.end();
   }
